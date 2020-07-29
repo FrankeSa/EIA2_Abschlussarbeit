483af73ad4d22437e10e9b71fd44b5250bf763cc
Sarah Franke <sarah.franke@hs-furtwangen.de>
2020-07-29 21:27:25 +0200

    mongo install

--- Objects/Objects.js
+++ Objects/Objects.js
@@ -2,16 +2,6 @@
 var Zaberbild;
 (function (Zaberbild) {
     class Objects {
-        constructor(_position) {
-            if (_position)
-                this.position = _position;
-            else
-                this.position = new Zaberbild.Vector(0, 0);
-            this.velocity = new Zaberbild.Vector(0, 0);
-        }
-        draw() {
-            // console.log ("Moveable draw");
-        }
     }
     Zaberbild.Objects = Objects;
 })(Zaberbild || (Zaberbild = {}));
