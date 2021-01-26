8cff65c53f06a1c3bb20daf1688205a103f4e6ec
Sarah Franke <sarah.franke@hs-furtwangen.de>
2021-01-26 12:01:19 +0100

    update

---
+++ Main/Main.js
@@ -0,0 +1,9 @@
+"use strict";
+var Firework;
+(function (Firework) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.group("HalloWelt");
+    }
+})(Firework || (Firework = {}));
+//# sourceMappingURL=Main.js.map
