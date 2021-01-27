bf8b1e269cee0932e48f7f7abe09d23669570b27
Sarah Franke <sarah.franke@hs-furtwangen.de>
2021-01-27 20:25:10 +0100

    some tests

--- Main/Main.js
+++ Main/Main.js
@@ -2,8 +2,33 @@
 var Firework;
 (function (Firework) {
     window.addEventListener("load", handleLoad);
+    let crc2;
     function handleLoad(_event) {
-        console.group("HalloWelt");
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2d");
+        //  let form: HTMLFormElement = <HTMLFormElement>document.querySelector("#form#controlPanel");
+        let saveBtn = document.querySelector("button#saveBtn");
+        let loadBtn = document.querySelector("button#loadBtn");
+        console.log("HalloWelt");
+        canvas.addEventListener("click", dontknow);
+        saveBtn.addEventListener("click", saveData);
+        loadBtn.addEventListener("click", loadData);
+    }
+    function dontknow(_event) {
+        let mousePositionX = _event.offsetX;
+        let mousepositionY = _event.offsetY;
+        console.log("x: ", mousePositionX, "y: ", mousepositionY);
+    }
+    function saveData(_event) {
+        let textArea = document.querySelector("input#textarea");
+        let fireworkTitel;
+        fireworkTitel = textArea.value;
+        console.log("FireworkTitel= ", fireworkTitel);
+    }
+    function loadData(_event) {
+        console.log("Datein wurden geladen");
     }
 })(Firework || (Firework = {}));
 //# sourceMappingURL=Main.js.map
--- Main/Main.ts
+++ Main/Main.ts
@@ -1,79 +1,107 @@
 namespace Firework {
-    window.addEventListener("load", handleLoad);
+  window.addEventListener("load", handleLoad);
+  let crc2: CanvasRenderingContext2D;
 
-    function handleLoad(_event: Event): void {
 
-  console.group("HalloWelt");
 
+  function handleLoad(_event: Event): void {
+    let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
+    if (!canvas)
+      return;
+    crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
+    //  let form: HTMLFormElement = <HTMLFormElement>document.querySelector("#form#controlPanel");
+    let saveBtn: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#saveBtn");
+    let loadBtn: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button#loadBtn");
+    console.log("HalloWelt");
+    canvas.addEventListener("click", dontknow);
+    saveBtn.addEventListener("click", saveData);
+    loadBtn.addEventListener("click", loadData);
 
-    }
+  }
 
 
 
 
+  function dontknow(_event: MouseEvent): void {
+    let mousePositionX: number = _event.offsetX;
+    let mousepositionY: number = _event.offsetY;
+    console.log("x: ", mousePositionX, "y: ", mousepositionY);
 
+  }
 
+  function saveData(_event: Event): void {
+    let textArea: HTMLInputElement = <HTMLInputElement>document.querySelector("input#textarea");
+    let fireworkTitel: string;
+    fireworkTitel = textArea.value;
+    console.log("FireworkTitel= ", fireworkTitel);
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function loadData(_event: Event): void {
     
+    console.log("Datein wurden geladen");
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
